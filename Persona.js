0d66d89b34676cb5b8216ea5c693f9a45ef08e11
DavidPerez29 <78055476+DavidPerez29@users.noreply.github.com>
2024-12-12 16:16:33 -0500

    @DavidPerez

---
+++ Persona.js
@@ -0,0 +1,23 @@
+class Persona{
+
+constructor (nombre,edad, sexo, profesion, correo, telefono, empresa){
+    this.nombre = nombre;
+    this.edad = edad;
+    this.sexo = sexo;
+    this.profesion = profesion;
+    this.correo = correo;
+    this.telefono;
+    this.empresa = empresa;
+
+
+}
+
+function consultarPersona(identificacion) {
+
+    return Persona.toString ;
+}
+   
+
+
+
+}
