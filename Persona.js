70697c1db8ed241038ce04cd0203ae7d371a59bd
DavidPerez29 <78055476+DavidPerez29@users.noreply.github.com>
2024-12-14 16:13:26 -0500

    @RobertRomero287

--- Persona.js
+++ Persona.js
@@ -1,23 +1,33 @@
 class Persona{
-
-constructor (nombre,edad, sexo, profesion, correo, telefono, empresa){
-    this.nombre = nombre;
-    this.edad = edad;
-    this.sexo = sexo;
-    this.profesion = profesion;
-    this.correo = correo;
-    this.telefono;
-    this.empresa = empresa;
-
-
-}
-
-function consultarPersona(identificacion) {
-
-    return Persona.toString ;
-}
-   
-
-
+    
+    constructor (identificacion,nombre,edad, sexo, profesion, correo, telefono, empresa, tipo_persona){
+        this.identificacion = identificacion;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.profesion = profesion;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.empresa = empresa;
+        this.tipo_persona  = tipo_persona;}
 
 }
+
+function crearPersona(identificacion,nombre, edad, sexo, profesion, correo, telefono, empresa, tipo_persona) {
+    return new Persona(identificacion,nombre, edad, sexo, profesion, correo, telefono, empresa, tipo_persona);
+}
+
+function eliminarPersona(identificacion) {
+    const index = personas.findIndex(persona => persona.identificacion === identificacion);
+    
+    if (index !== -1) {
+        personas.splice(index, 1);
+        console.log(`Persona con numero de identificacion ${identificacion} fue eliminada.`);
+    } else {
+        console.log(`Persona con Cedula ${correo} no fue encontrada.`);
+    }
+}
+
+const persona1 = crearPersona('Admin', 30, 'Masculino', 'Desarrollador', 'david.perez@gmail.com', '1234567890', 'TechCorp', 'Empleado');
+console.log(persona1);
+
